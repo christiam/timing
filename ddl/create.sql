1bcbd9eb2c04466a8e5730b380cbc6e059ddf8eb
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2015-03-20 18:23:32 -0400

    Add finished_at column

--- ddl/create.sql
+++ ddl/create.sql
@@ -3,5 +3,12 @@ CREATE TABLE IF NOT EXISTS runtime (
     ellapsed_time   FLOAT CHECK(ellapsed_time >= 0.0),
     system_time     FLOAT CHECK(system_time >= 0.0),
     user_time       FLOAT CHECK(user_time >= 0.0),
-    pcpu            INTEGER CHECK(pcpu >= 0)
+    pcpu            INTEGER CHECK(pcpu >= 0),
+    finished_at     TEXT DEFAULT ''
 );
+CREATE TRIGGER IF NOT EXISTS finished_at_trigger AFTER INSERT ON runtime
+BEGIN
+    UPDATE runtime
+    SET finished_at = datetime('now', 'localtime')
+    WHERE rowid = NEW.rowid;
+END;
--- ddl/insert.sql
+++ ddl/insert.sql
@@ -1,2 +1,2 @@
-INSERT INTO runtime VALUES("foo", "1", "2", "3", "90");
+INSERT INTO runtime VALUES("foo", "1", "2", "3", "90", '');
 /*INSERT INTO runtime VALUES("bar", "-1", "2", "3", "99");*/
