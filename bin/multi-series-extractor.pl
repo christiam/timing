c9712f8ca6a2e8eb2883591417c53fdb72acded5
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2015-04-16 18:14:57 -0400

    Generalize multi-series-extractor.pl, add check for exit code

--- bin/multi-series-extractor.pl
+++ bin/multi-series-extractor.pl
@@ -3,22 +3,26 @@ use strict;
 use warnings;
 
 my %data;
-my @QUERIES = qw(1336q 15795q 139340q);
+my @TEST_CASES = qw(blastx_on_mesos blastx_standalone);
 while (<>) {
     chomp;
-    if (/^(\d+q)-(\d+)\|/) {
-        $data{$2}{$1} = [ split(/\|/) ] ->[1];
+    if (/^(.*)-(\d+)\|/) {
+		my @fields = split(/\|/);
+		if ($fields[5] == 0) {	# check exit code
+			$data{$2}{$1} = $fields[1];	# save the ellapsed time
+		}
         #print "saved time for $1 ", $data{$2}{$1}, "\n";
     } 
 }
 
 $\ = "\n", $, = "\t";
 # N.B.: Produces data to be used by gnuplot >= 4.6 (this is used with set key autotitle columnhead)
-print "Number of cores", "1336 queries", "15795 queries", "139340 queries";
-foreach my $num_cores (sort {$a<=>$b} keys %data) {
+print "Number of cores", @TEST_CASES;
+foreach my $n (sort {$a<=>$b} keys %data) {
     my @times;
-    foreach my $q (@QUERIES) {
-        push @times, $data{$num_cores}{$q};
+    foreach my $t (@TEST_CASES) {
+        my $measurement = (exists($data{$n}{$t})) ? $data{$n}{$t} : "-";
+        push @times, $measurement;
     }
-    print $num_cores, @times;
+    print $n, @times;
 }
