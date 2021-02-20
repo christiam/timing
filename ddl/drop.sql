7494edd061eacca465b6c6a05a6ad20451e189c9
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2021-02-20 18:56:00 -0500

    Drop views also

--- ddl/drop.sql
+++ ddl/drop.sql
@@ -2,3 +2,5 @@ PRAGMA foreign_keys = ON;
 DROP TABLE IF EXISTS host_info;
 DROP TABLE IF EXISTS runtime;
 DROP TABLE IF EXISTS system_info;
+DROP VIEW IF EXISTS runtime_view;
+DROP VIEW IF EXISTS system_info_view;
