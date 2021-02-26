ca376b51764b59b46d3e9986c60def041a49eb85
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2021-02-26 09:38:32 -0500

    Bump version

--- version.pl
+++ version.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-our $VERSION = '1.2.0';
+our $VERSION = '1.2.1';
 # Must be a single integer, stored in https://www.sqlite.org/pragma.html#pragma_user_version
-our $DB_VERSION = 3;
+our $DB_VERSION = 4;
