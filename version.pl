69dde3c01572234c31a3547aaa397d0fe775d8f0
Christiam Camacho <camacho@ncbi.nlm.nih.gov>
2021-02-26 09:20:46 -0500

    Bump versions, add total_memory to system_info_view

--- version.pl
+++ version.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-our $VERSION = '1.1.0';
+our $VERSION = '1.2.0';
 # Must be a single integer, stored in https://www.sqlite.org/pragma.html#pragma_user_version
-our $DB_VERSION = 2;
+our $DB_VERSION = 3;
